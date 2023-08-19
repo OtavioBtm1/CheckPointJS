59c3c6216e20770a8e8750b1eb0d765da2c117b9
OtavioBtm1 <rm552012@fiap.com.br>
2023-08-18 22:45:57 -0300

    Adição Exercicio 1 completo

--- Exercicio1.js
+++ Exercicio1.js
@@ -1,9 +1,20 @@
 const salarios=[1000,4000,200,4000,6000,2000,1500,1750,650,10000]
 const acrescimos=salarios.map((salario)=>{
     if (salario<2000){
-        return salario*0.15
+        return salario= salario + salario*0.15
     }else if (salario>=2000){
-        return salario*0.10
+        return salario= salario + salario*0.10
     }
 })
+const superior = acrescimos.filter(salario => {
+    if(salario>2500){
+        return salario
+    }
+});
+function soma(v1, v2){
+    return v1 + v2
+}
+const total=superior.reduce(soma)
+console.log(total)
+console.log(superior)
 console.log(acrescimos)
